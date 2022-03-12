e13b9c0ac9a76315266f25926a6b74cf897e0603
Indira Djambaeva <indira.djambaeva.kk@gmail.com>
2022-03-12 20:11:59 +0600

    Add REST API

--- external-api/js.js
+++ external-api/js.js
@@ -34,7 +34,42 @@
 // }
 
 
-//Ex2
+// //Ex2
+// document.getElementById("get-jokes"). addEventListener("click", getJokes);
+
+// function getJokes (e) {
+//   const numberOfJokes = document.getElementById("number").value;
+
+//   const xhr = new XMLHttpRequest();
+
+//   xhr.open("GET", `http://api.icndb.com/jokes/random/${numberOfJokes}`, true);
+
+//   xhr.onload = function() {
+//     if(this.status === 200) {
+//       const res = JSON.parse(this.response);
+
+//       let output = "";
+//       if(res.type === "success") {
+//         res.value.forEach((item) => {
+//           output += `<li>${item.joke}</li>`
+          
+//         });
+//       } else {
+//         output += `<li>Error</li>`
+//       }
+
+//       document.getElementById("jokes").innerHTML = output;
+
+//       console.log(res);
+//     }
+//   };
+
+//   xhr.send();
+//   e.preventDefault();
+// }
+
+
+//Ex3
 document.getElementById("get-jokes"). addEventListener("click", getJokes);
 
 function getJokes (e) {
@@ -42,7 +77,7 @@ function getJokes (e) {
 
   const xhr = new XMLHttpRequest();
 
-  xhr.open("GET", `http://api.icndb.com/jokes/random/${numberOfJokes}`, true);
+  xhr.open("GET", `http://api.icndb.com/categories/${numberOfJokes}`, true);
 
   xhr.onload = function() {
     if(this.status === 200) {
---
+++ rest-api/js.js
@@ -0,0 +1,141 @@
+// GET POST PUT DELETE
+
+// A - Application P - Programming I - Interface
+
+// GET - // https://api.myapp.com/users - Get All users
+// GET - https://api.myapp.com/users/1 - Get Single user
+// POST - https://api.myapp.com/users - Add user
+// PUT - https://api.myapp.com/users/1 - Update user
+// DELETE - https://api.myapp.com/users/1 - Delete user
+
+// //Ex1
+// const posts = [
+//   {
+//     title: "Post 1 title",
+//     body: "Post 1 body text",
+//   },
+//   {
+//     title: "Post 2 title",
+//     body: "Post 2 body text",
+//   },
+//   {
+
+//     title: "Post 3 title",
+//     body: "Post 3 body text",
+//   },
+// ];
+
+// function getPosts() {
+//   setTimeout(() => {
+//     posts.push(post);
+//   }, 1000);
+// }
+
+// console.log(posts);
+
+// //Ex2
+// const posts = [
+//   {
+//     title: "Post 1 title",
+//     body: "Post 1 body text",
+//   },
+//   {
+//     title: "Post 2 title",
+//     body: "Post 2 body text",
+//   },
+//   {
+//     title: "Post 3 title",
+//     body: "Post 3 body text",
+//   },
+// ];
+
+// function getPosts() {
+//   posts.forEach(item => {
+//     console.log(item);
+//   });
+// }
+
+// getPosts();
+// console.log(posts);
+
+// //Ex3
+// const posts = [
+//   {
+//     title: "Post 1 title",
+//     body: "Post 1 body text",
+//   },
+//   {
+//     title: "Post 2 title",
+//     body: "Post 2 body text",
+//   },
+//   {
+//     title: "Post 3 title",
+//     body: "Post 3 body text",
+//   },
+// ];
+
+// function createPost(post) {
+//   setTimeout(() => {
+//     posts.push(post);
+//   }, 300);
+// }
+
+// function getPosts() {
+//   setTimeout(() => {
+//     let output = "";
+
+//     posts.forEach((post) => {
+//       output += `<li>${post.title}</li>`;
+//     });
+
+//     document.body.innerHTML = output;
+//   }, 1000);
+// }
+
+// createPost({title: "Post 4 title", body: "Post 4 bosy text"});
+
+// getPosts();
+
+// console.log(posts);
+
+
+//Ex3
+const posts = [
+  {
+    title: "Post 1 title",
+    body: "Post 1 body text",
+  },
+  {
+    title: "Post 2 title",
+    body: "Post 2 body text",
+  },
+  {
+    title: "Post 3 title",
+    body: "Post 3 body text",
+  },
+];
+
+function createPost(post, callback) {
+    setTimeout(() => {
+      posts.push(post);
+      callback();
+    }, 3000);
+  }
+
+function getPosts() {
+    setTimeout(() => {
+      let output = "";
+  
+      posts.forEach((post) => {
+        output += `<li>${post.title}</li>`;
+      });
+  
+  document.body.innerHTML = output;
+  }, 1000);
+}
+
+createPost({title: "Post 4 title", body: "Post 4 bosy text"}, getPosts);
+
+getPosts();
+
+// console.log(posts);
